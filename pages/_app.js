36da4c80aff0a7bc122666b3721681fbd81e5b3f
ardfak <fhckfy-fhckfy@mail.ru>
2020-07-09 11:47:30 +0000

    Loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/main.scss'
+import NextProgress from 'nextjs-progressbar'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextProgress
+        color='#29D'
+        startPosition='0.3'
+        stopDelayMs='200'
+        height='3'
+      />
       <Component {...pageProps} />
     </>
   )
